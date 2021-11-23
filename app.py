1558597d361bc0de1dd286bc4588a83fd37fe476
Gilberto Mejía Carrazco <17460368@colima.tecnm.mx>
2021-11-23 14:26:07 -0600

    Primer Commit

---
+++ app.py
@@ -0,0 +1,85 @@
+from flask import Flask, render_template, request
+from flask_sqlalchemy import SQLAlchemy
+from werkzeug.utils import redirect
+
+app = Flask(__name__)
+#Habilitando el uso del ORM en la app flask mediante el objeto "db"
+db = SQLAlchemy(app)
+
+#postgresql://<nombre_usuario>:<password>@<host>:<puerto>/<nombre_basededatos>
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:PostRei25@localhost:5432/notas'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']  = False
+
+class Notas(db.Model):
+    '''Clase Notas'''
+    __tablename__ = "notas"
+    idNota = db.Column(db.Integer, primary_key = True)
+    tituloNota = db.Column(db.String(80))
+    cuerpoNota = db.Column(db.String(150))
+
+    def __init__(self,tituloNota,cuerpoNota):
+        self.tituloNota = tituloNota
+        self.cuerpoNota = cuerpoNota
+
+@app.route('/')
+def index():
+    return render_template ("index.html")
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+@app.route("/crearnota", methods=['POST'])
+def crearnota():
+    campotitulo = request.form["campotitulo"]
+    campocuerpo = request.form["campocuerpo"]
+    print(campotitulo)
+    print(campocuerpo)
+    notaNueva = Notas(tituloNota=campotitulo,cuerpoNota=campocuerpo)
+    db.session.add(notaNueva)
+    db.session.commit()
+    return redirect("/leernotas")
+    #return render_template("index.html", titulo = campotitulo, cuerpo = campocuerpo)
+
+@app.route("/leernotas")
+def leernotas():
+    consulta_notas = Notas.query.all()
+    print(consulta_notas)
+    for nota in consulta_notas:
+        titulo = nota.tituloNota;
+        cuerpo = nota.cuerpoNota;
+        print(nota.tituloNota)
+        print(nota.cuerpoNota)
+
+    return render_template("leernotas.html", consulta = consulta_notas)
+
+@app.route("/eliminarnota/<id>")
+def eliminar(id):
+    nota = Notas.query.filter_by(idNota = int(id)).delete()
+    print(nota)
+    db.session.commit()
+    return redirect("/leernotas")
+
+@app.route("/editarnota/<id>")
+def editar(id):
+    nota = Notas.query.filter_by(idNota = int(id)).first()
+    print(nota)
+    print(nota.tituloNota)
+    print(nota.cuerpoNota)
+    return render_template("modificar.html", nota = nota)
+
+@app.route("/modificar", methods=['POST'])
+def modificar():
+    idnota = request.form["idnota"]
+    nuevo_titulo = request.form["campotitulo"]
+    nuevo_cuerpo = request.form["campocuerpo"]
+    nota = Notas.query.filter_by(idNota = int(idnota)).first()
+    nota.tituloNota = nuevo_titulo
+    nota.cuerpoNota = nuevo_cuerpo
+    db.session.commit()
+    return redirect("/leernotas")
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run()
+    
